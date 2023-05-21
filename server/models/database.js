d3a39425886aa153f1ee90018fb55273cd4b1ef9
bilal2912112 <bilaliqbal2912112@gmail.com>
2023-05-21 23:42:57 +0500

    adding data base configration

--- server/models/database.js
+++ server/models/database.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 mongoose.connect(process.env.Mongodb_uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const db = mongoose.connection;
+const db = mongoose.createConnection();
 db.on("error", console.error.bind(console, "connection error with database"));
 db.once("open", function () {
   console.log("connected");
 });
 require("./Recipe");
+
